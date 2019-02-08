c4af9bb773955516114df9b99a99397a1abe8871
myunghun choi <hxc9272@g.rit.edu>
2019-02-08 11:38:20 -0500

    1

---
+++ Choi_PE8/Choi_PE8/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Choi_PE8
+{
+    class Car
+    {
+        public int year;
+        public string make;
+        public string model;
+        public float gasLevel;
+
+        public Car()
+        {
+            year = 2000;
+            make = "Ford";
+            model = "car";
+            gasLevel = 20;
+        }
+        public Car(int year, string make, string model)
+        {
+            if(year > 2000 && year < 2019)
+            {
+                this.year = year;
+            }
+            this.make = make;
+            this.model = model;
+        }
+        public void Drive()
+        {
+            Console.WriteLine("Lets Go!");
+            gasLevel -= 1;
+        }
+        public float CheckGas()
+        {
+            return gasLevel;
+        }
+        public void FillUp(float amount)
+        {
+            gasLevel += amount;
+        }
+    }
+}
---
+++ Choi_PE8/Choi_PE8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Choi_PE8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // only declares
+            Car myCar;
+            // only init
+            myCar = new Car(2018, "Kia", "Optima");
+            //myCar.year = 2018;
+            //myCar.make = "Kia";
+            //myCar.model = "Optima";
+
+            Car yourCar = new Car();
+
+            //myCar.year = 2019;
+            int myMoney = 1000000;
+
+            //Console.WriteLine(myCar.make);
+            //Console.WriteLine(myMoney / myCar.make);
+
+            if(myCar.CheckGas() > 0)
+            {
+                myCar.Drive();
+            }
+            
+        }
+    }
+}
